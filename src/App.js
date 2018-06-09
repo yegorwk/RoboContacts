47343609094a3dff083aed9b8bb0e4f84c9bb593
yegorwk <yegorwk@gmail.com>
2018-06-09 11:48:24 +0500

    Added scrollbox

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Scroll from './Scroll';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
@@ -31,7 +32,9 @@ class App extends Component {
 			<div className = 'tc'>
 			  <h1 className='f1'>RoboFriends</h1>
 			  <SearchBox searchChange = {this.onSearchChange}/>
-			  <CardList robots = {filteredRobots} />
+			  <Scroll>
+			  	<CardList robots = {filteredRobots} />
+			  </Scroll>
 		</div>
 		);
     }
